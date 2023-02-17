0b7b45e27a313171d90cd2243f4afa132caf3024
Gary Grant <garygrant693@gmail.com>
2023-02-16 23:42:59 -0500

    added morgan as middleware

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-//morgan 
+const morgan = require('morgan')
 const port = 8080;
 
 const app = express();
+
+//middleware 
+app.use(morgan('dev'));
+app.use(express.json());
+//express.static
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)

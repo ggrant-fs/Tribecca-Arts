24a875fd65d7c37a707d74306def44ab736779f5
Gary Grant <garygrant693@gmail.com>
2023-02-20 00:49:32 -0500

    created controller

---
+++ controllers/sculpturesController.js
@@ -0,0 +1,29 @@
+const sculptures = require("../models/sculptures");
+
+exports.getAllSculptures = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"this route has not yet been defined"
+    });
+}
+
+exports.getSculpture = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"this route has not yet been defined"
+    });
+}
+
+exports.createSculpture = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"this route has not yet been defined"
+    });
+}
+
+exports.deleteSculpture = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"this route has not yet been defined"
+    });
+}

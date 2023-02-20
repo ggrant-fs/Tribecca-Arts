9629c8a06d39eb7ce05ee3bc6495b94f7f1e3486
Gary Grant <garygrant693@gmail.com>
2023-02-20 02:36:55 -0500

    renamed file names

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const morgan = require('morgan')
-const jewelryRouter =require('./routes/jewelry');
-const paintingsRouter = require('./routes/paintings');
-const sculptureRouter = require('./routes/sculptures');
+const jewelryRouter =require('./routes/jewelryRoutes');
+const paintingsRouter = require('./routes/paintingsRoutes');
+const sculptureRouter = require('./routes/sculpturesRoutes');
 
 const app = express();
 

4d52051f165861ade69d52cbacc91c351956cedf
Gary Grant <garygrant693@gmail.com>
2023-02-20 00:06:31 -0500

    created controllers

---
+++ controllers/paintingsController.js
@@ -0,0 +1,37 @@
+const paintings = require('../models/paintings');
+
+
+exports.getAllPaintings = async(req,res)=>{
+   res.status(500).json({
+     status:"Error",
+     message:"This route has not yet been defined"
+   });
+}
+
+exports.getPainting = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"This route has not yet been defined"
+    });
+}
+
+exports.createPainting = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"This route has not yet been defined"
+    });
+}
+
+exports.updatePainting = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"This route has not yet been defined"
+    });
+}
+
+exports.deletePainting = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"This route has not yet been defined"
+    });
+}

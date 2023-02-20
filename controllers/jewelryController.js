442407f0b71fe8132b2766a3e3dc698bfdf16b8d
Gary Grant <garygrant693@gmail.com>
2023-02-20 00:16:08 -0500

    added controller

---
+++ controllers/jewelryController.js
@@ -0,0 +1,36 @@
+const jewelry = require('../models/jewelry');
+
+exports.getAllJewelry = async(req,res)=>{
+    res.status(500).json({
+        status:"Error",
+        message:"This route has not yet been defined"
+    });
+}
+
+exports.getJewelry = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"this route has not yet been defined"
+    });
+}
+
+exports.createJewelry = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"This route has not yet been defined"
+    });
+}
+
+exports.updateJewelry = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"this route has not yet been defined"
+    });
+}
+
+exports.deleteJewelry = async(req,res) =>{
+    res.status(500).json({
+        status:"Error",
+        message:"this route has not yet been defined"
+    })
+}

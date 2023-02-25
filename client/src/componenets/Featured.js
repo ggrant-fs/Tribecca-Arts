5d8ef154ea52d11d351b82e57c0064de0396588e
Gary Grant <garygrant693@gmail.com>
2023-02-25 00:28:07 -0500

    crearted new compoenent "Featured.js"

---
+++ client/src/componenets/Featured.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Featured =(props)=>{
+    return(
+       <div></div>
+    )
+}
+
+export default Featured;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,11 @@
 import React,{Fragment} from 'react';
 import Layout from '../componenets/Layout';
-// import Banner from '../componenets/Banner';
 import photo from '../images/banner.jpg';
 import Sectional from '../componenets/Sectional';
 const Home =(props)=>{
     return(
        <Fragment>
          <Layout image={photo}>
-         {/* <Banner image={photo}/> */}
         <Sectional/>
          </Layout>
        </Fragment>

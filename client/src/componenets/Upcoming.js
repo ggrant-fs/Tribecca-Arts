e3607ac74ffc2c591e449c5e83906c1baa4acc06
Gary Grant <garygrant693@gmail.com>
2023-02-25 22:53:22 -0500

    added the default width and height of the flexbox items into variables

--- client/src/componenets/Upcoming.js
+++ client/src/componenets/Upcoming.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Upcoming =(props)=>{
     return (
         <div>
-
+          <div className="upcoming-auctions">
+            <div className="upcoming__date"></div>
+            <div className="upcoming__date"></div>
+          </div>
         </div>
     )
 }

81e340b7efb5eac32a58b807125f61d8a6fc5788
Gary Grant <garygrant693@gmail.com>
2023-03-03 22:51:30 -0500

    added images of paintings to featured-container items

--- client/src/componenets/Featured.js
+++ client/src/componenets/Featured.js
@@ -1,12 +1,21 @@
 import React,{Fragment} from 'react';
+import painting from '../images/featuredpainting.jpg';
+import painting2 from '../images/featuredpainting2.jpg';
+import painting3 from '../images/featuredpainting3.jpg';
 
 const Featured =(props)=>{
     return(
        <Fragment>
        <div className='featured-container'>
-         <div className='featured__jewelry'></div>
-         <div className='featured__paintings'></div>
-         <div className='featured__sculptures'></div>
+         <div className='featured__jewelry'>
+         <img src={painting2} alt='' className='featured__img'/>
+         </div>
+         <div className='featured__paintings'>
+          <img src={painting} alt='' className='featured__img'/>
+         </div>
+         <div className='featured__sculptures'>
+         <img src={painting3} alt='' className='featured__img'/>
+         </div>
        </div>
        </Fragment>
     )

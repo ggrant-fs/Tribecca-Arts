8af3c50068603c0dd85928619f96cfa242396869
Gary Grant <garygrant693@gmail.com>
2023-03-30 19:00:14 -0400

    added lorem ipsum to paragraphs

--- client/src/componenets/Exhibitions.js
+++ client/src/componenets/Exhibitions.js
@@ -10,29 +10,50 @@ const Exhibitions =(props)=>{
             <div className="exhibition__img_wrapper">
               <img src={exhibition1} alt='' className='exhibition__img'/>
               <div className='exhibition__description'>
-                <h1 className='exhibition__title'>title of exhibition</h1>
-                <p>
-                  description of the exhibition
+                <h1 className='exhibition__title'>Andy Worhal</h1>
+                <p className='exhibition__paragraph'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                culpa qui officia deserunt mollit anim id est laborum.
                 </p>
               </div>
               <hr/>
             </div>
+          
+   {/* ==================================================================================   */}
             <div className="exhibition__img_wrapper">
               <img src={exhibition2} alt='' className='exhibition__img'/>
               <div className='exhibition__description'>
-                <h1 className='exhibition__title'>title of exhibition</h1>
-                <p>
-                  description of the exhibition
+                <h1 className='exhibition__title'>Banksy</h1>
+                <p className='exhibition__paragraph'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                culpa qui officia deserunt mollit anim id est laborum.
                 </p>
               </div>
               <hr/>
             </div>
+   {/* ==================================================================================          */}
             <div className="exhibition__img_wrapper">
               <img src={exhibition3} alt='' className='exhibition__img'/>
               <div className='exhibition__description'>
-                <h1 className='exhibition__title'>title of exhibition</h1>
-                <p>
-                  description of the exhibition
+                <h1 className='exhibition__title'>Helen Frankenthaler</h1>
+                <p className='exhibition__paragraph'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                culpa qui officia deserunt mollit anim id est laborum.
                 </p>
               </div>
             </div>

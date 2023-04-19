5b2a8a8e88dd1fb029149889ec276c828bf30a92
Gary Grant <garygrant693@gmail.com>
2023-04-19 00:40:02 -0400

    added title

--- client/src/componenets/Featured.js
+++ client/src/componenets/Featured.js
@@ -1,17 +1,17 @@
 import React,{Fragment} from 'react';
-import painting from '../images/featuredpainting.jpg';
-import painting2 from '../images/featuredpainting2.jpg';
-import painting3 from '../images/featuredpainting3.jpg';
+import painting1 from '../images/van_gogh_starry_night.png';
+import painting2 from '../images/van_gogh.jpeg';
+import painting3 from '../images/Van-Gogh_Sunflowers.png';
 
 const Featured =(props)=>{
     return(
        <Fragment>
        <div className='featured-container'>
          <div className='featured__paintings'>
-         <img src={painting2} alt='' className='featured__img'/>
+         <img src={painting1} alt='' className='featured__img'/>
          </div>
          <div className='featured__paintings'>
-          <img src={painting} alt='' className='featured__img'/>
+          <img src={painting2} alt='' className='featured__img'/>
          </div>
          <div className='featured__paintings'>
          <img src={painting3} alt='' className='featured__img'/>
--- client/src/componenets/Header.js
+++ client/src/componenets/Header.js
@@ -5,6 +5,7 @@ import Nav from './Nav';
 const Header = (props) =>{
     return(
        <div className="main-header">
+        <h1 className="main-header__title">Tribecca Arts</h1>
          <Nav/>
        </div>
     )

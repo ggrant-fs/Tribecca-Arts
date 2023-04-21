03af5a786577b7c3dab06db48a53c22717da4ccf
Gary Grant <garygrant693@gmail.com>
2023-04-20 23:04:41 -0400

    added nav items

--- client/src/componenets/Nav.js
+++ client/src/componenets/Nav.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Fragment } from 'react';
 
 const Nav =(props) =>{
     return (
-       <div></div>
+       <Fragment>
+        <ul className='nav-list'>
+            <li className='nav__item'>Home</li>
+            <li className='nav__item'>Art</li>
+            <li className='nav__item'>Tickets</li>
+            <li className='nav__item'>Member</li>
+        </ul>
+       </Fragment>
     )
 }
 
---
+++ client/src/pages/Art.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../componenets/Layout'
+const Art = () => {
+  return (
+   <Layout>
+    
+   </Layout>
+  )
+}
+
+export default Art

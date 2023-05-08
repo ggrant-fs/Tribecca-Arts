6fb7e2fb099fc1e53e0b48d50c8fc516f4bf1750
Gary Grant <garygrant693@gmail.com>
2023-05-08 01:30:09 -0400

    added api middleware to configureStore

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,13 +1,18 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore,getDefaultMiddleware} from '@reduxjs/toolkit';
 import artReducer from './art';
 import signUpReducer from './signUp';
+import api from './middleware/api';
 
 
 const store = configureStore({
      reducer:{
         art:artReducer,
         signup: signUpReducer
-     }
+     },
+     middleware:(getDefaultMiddleware)=>[
+      ...getDefaultMiddleware,
+      api
+     ]
 })
 
 
